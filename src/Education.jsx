5cf6be9c5b16866b19203d9ec08f53876a4cd42c
vraj89 <vrajrajeshbhaivaghela@gmail.com>
2026-05-08 19:32:35 +0530

    fix: Add default exports to Education and Skills components

--- src/Education.jsx
+++ src/Education.jsx
@@ -33,3 +33,5 @@ export const Education = () => {
     </section>
   );
 };
+
+export default Education;
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -32,3 +32,5 @@ export const Skills = () => {
     </section>
   );
 };
+
+export default Skills;
